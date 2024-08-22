3a5aa0206675d1d1c362f3d7f5a96a60eda3f8f8
Bobbythecoder2034 <kkoala110@gmail.com>
2024-08-21 18:22:27 -0700

    First Repo Notes
    Year-1 Python and JS Notes

---
+++ JS/js-basics.js
@@ -0,0 +1,104 @@
+let park = "disney"
+
+//Camel Case Example
+let firstName = "Kaden"
+let lastName = "Smiley"
+
+console.log(firstName)
+console.log(park)
+console.log(typeof (-65))
+console.log(typeof(20n))
+console.log(typeof "true")
+
+let message = `I enjoy
+chocolate
+and 
+Ice Cream`
+console.log(message)
+
+let name = "Kaden"
+let age = 16
+let alive = true
+let favFoods = ["mexican", "italian", "chinese"]
+
+console.log(name + " " + age + " " + alive + " " + favFoods)
+
+let result = "Hired"
+const nickName = "The Council"
+console.log("The person we interviewed yesterday named " + nickName + " was " + result)
+// spaces between + and ""s don't matter
+
+let turquoise = "blue"
+const strandsOfGrass = 1000000
+var dead = true
+console.log("The sky was "+turquoise+" and there were "+strandsOfGrass+" strands of grass that spoke "+dead)
+
+console.log(typeof String(84))
+console.log(String(84))
+console.log(String(null))
+console.log(String(true))
+console.log(Number(true)+" "+Number(null))
+console.log(Boolean(0))
+let age1 = 23
+let age2 = 56
+if(age1>45){
+    console.log("They OLD")
+}
+// Since this if statement is false, nothing is printed
+let bf = "Jesse"
+let thinksBF = "Dylan"
+if(bf!=thinksBF){
+    console.log("You need more friends")
+}
+if(15 == "15"){
+    console.log("You can read yay")
+}
+//two equal signs will convert and then compare. 3 equal signs won't work becuase there is no conversion.
+if("15" === "15"){
+    console.log("This won't work unless they are the same type of data")
+}
+let test1, test2, test3
+//These are all separate variables
+let aliveOrNot = false, year = 2024, truck = "Tacoma"
+if(aliveOrNot == true){
+    if(year==2024){
+        console.log("Yayy")
+    }
+    console.log("Level 1")
+}else{
+    console.log("oops")
+}
+if(aliveOrNot){
+    console.log("True")
+} // This will run because alive is a boolean that is set to true. If it was false, it wouldn't run
+let money = 14
+if(money <= 20){
+    console.log("Broke Netflix")
+}else if(money >= 21 && money <= 60){
+    console.log("Movie Theatre")
+}else if(money >= 61 && money <= 80){
+    console.log("Dinner")
+}else{
+    console.log("Sky Diving")
+}
+
+//Browser Commands
+// let num = 5546
+// alert("This is proof")
+// console.log("I will live until I'm " + num)
+// This will stop at the alert until the user confirms they read it, then executes the rest of the code after it.
+
+// answer = confirm("ME")
+// if the user responds with okay, answer stores true and if it is a no, it will store false
+
+// let firstName = prompt("What is your first name?")
+// first name now holds the string value of the response
+// console.log(firstName)
+
+input1 = 76.8
+input2 = 5
+
+console.log(`Comparing ${input1} (${typeof(input1)}) amd ${input2} (${typeof(input2)})`)
+console.log(`When compared with == is ${input1 == input2}`)
+console.log(`When compared with === is ${input1 === input2}`)
+console.log(`When compared with Object.is is ${Object.is(input1,input2)}`)
---
+++ Python/python-basics.py
@@ -0,0 +1,98 @@
+school = "west-mec"
+
+#Snake Case example
+first_name = "Kaden"
+last_name = "Smiley"
+
+print(first_name)
+print(last_name)
+print(school)
+print(type(school))
+print(type (first_name))
+
+message = """
+food
+and games
+are cool"""
+print(message)
+
+age = 16
+alive = True
+fav_foods = ['mexican', "italian", 'chinese']
+print(first_name + " " + str(age) + " " + str(alive) + " " + str(fav_foods))
+meat = True
+dairy = "better"
+samsung = 578
+print(str(meat)+" "+dairy+" "+str(samsung))
+print(str(84))
+print(str(True))
+print(type(str(True)))
+print(int(True) + int(False) + float("55.5"))
+print(bool(1))
+
+age_1 = 23
+age_2 = 56
+if age_2 > 45:
+    print("They OLD")
+if age_2 == age_1:
+    print("Mission Failed")
+if age_1 != age_2:
+    print("Success")
+if age_1 <= age_1 + age_2:
+    print("Math")
+best_friend = "Zach"
+thinks_bf = "Tuesday"
+if best_friend != thinks_bf:
+    print("Good Jorb")
+if("25" == str(25)):
+   print("You have to convert before hand for Python, because it doesn't auto convert")
+
+alive = False
+year = 2024
+time = 4.7
+
+if alive == True & year == 2024:
+    print("YIPPEEE")
+if alive:
+    if year == 2024:
+        print("Better")
+    print("Me")
+else:
+    print("They have failed you")
+money = 39
+if money <= 20:
+    print("Broke Netflix")
+elif money >= 21 & money <= 60:
+    print("Movie theatre")
+elif money >= 61 & money <= 80:
+    print("Dinner")
+else:
+    print("Sky Diving")
+
+"""User Interactions"""
+"""first_name = input("What is your first name?")
+print("Good Morning " + input("What is your name? "))"""
+"""This works, It is in comments to make the rest of the code run automatically"""
+"""
+time = input("How many hours did the ride take? ")
+speed = input("What was the speed? ")
+distance = int(time) * int(speed)
+
+print(("The speed was " + str(speed) + "mph and the time was " + str(time) + " hours. The total distance was " + str(distance) + " miles."))
+
+speed = float(input("How fast in MPH?"))
+easy way to convert inputs directly into numbers, in this case, float means it can take decimals.
+"""
+"""
+CALCULATING THE HYPOTENUSE OF A TRIANGLE WITH THE PYTHAGOREAN THEOREM
+
+side_A = float(input("What is the length of side A: "))
+side_B = float(input("What is the length of side B: "))
+side_A *= side_A
+side_B *= side_B
+side_C = side_A+side_B
+side_C = side_C ** float(1/2)
+print("The hypotenuse of the triangle is " + str(side_C) + "cm")
+
+"""
+
