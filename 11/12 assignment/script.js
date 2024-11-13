7c356e3915ea42eadfafae0a4c141a9dd0fdd87a
Bobbythecoder2034 <kkoala110@gmail.com>
2024-11-12 20:28:48 -0700

    why

--- 11/12 assignment/script.js
+++ 11/12 assignment/script.js
@@ -1,4 +1,30 @@
 const body = document.getElementById('body').classList
-darkModeToggle(){
-    body.toggle("lightmode")
+darkModeToggle()
+{
+    body.toggle("base");
+}
+const hello = document.getElementById('1').classList
+bigHi()
+{
+    hello.toggle("base");
+}
+const bye = document.getElementById('2').classList
+goodBye()
+{
+    bye.toggle("base");
+}
+const big = document.getElementById('3').classList
+larger()
+{
+    big.toggle("base");
+}
+const small = document.getElementById('4').classList
+smaller()
+{
+    small.toggle("base");
+}
+const yellow = document.getElementById('5').classList
+yellowAndBlue()
+{
+    yellow.toggle("base");
 }
